75abb79b9481fc5e067f8013503cebd98a5aabb9
chikwanda <chikwanda.chisha.26@dartmouth.edu>
2025-02-13 00:14:06 -0500

    fixing deployment

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
   },
 });
